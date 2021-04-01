6550923c60952115cad3b27e82a55e6dad44618f
Adam Allevato <adam-pensa@user.noreply.github.com>
2021-04-01 14:31:47 -0500

    further simplified mwe, all in one world now

--- not_working.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-export GAZEBO_RESOURCE_PATH=$DIR:/usr/share/gazebo-11:$GAZEBO_RESOURCE_PATH
-export GAZEBO_MODEL_PATH=$DIR:$GAZEBO_MODEL_PATH
-
-gazebo not_working.world --verbose
