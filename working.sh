cfaf48b331bd2e0c84f18129df5804788cb4f2b9
Adam Allevato <adam-pensa@user.noreply.github.com>
2021-04-01 14:05:27 -0500

    make mwe clearer

--- working.sh
+++ working.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export GAZEBO_RESOURCE_PATH=$DIR:/usr/share/gazebo-11:$GAZEBO_RESOURCE_PATH
 export GAZEBO_MODEL_PATH=$DIR:$GAZEBO_MODEL_PATH
 
-gazebo world.world --verbose
+gazebo working.world --verbose
